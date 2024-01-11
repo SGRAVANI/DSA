559a5c5bc8828318b15699ccde9e243eb2cdef8b
SGRAVANI <shakuntla.ravani@gmail.com>
2024-01-12 01:57:22 +0530

    optimized knapsack

---
+++ DSA/DSA-5/knapsack/sumofsubset/minDifferenceSubset/CountNumberOfSubset/CountSubsetWithGivenDifference.js
@@ -0,0 +1,7 @@
+//this program will count number of subset with given difference
+//input arr=[1,1,2,3]
+//difference=1
+//subsets
+//p1={1,1,2} p2={3} diff=4-3=1
+//p1={1,3} p2={1,2} diff=1
+//p1={1,3} p2={1,2} diff=1  position/index of 1 interchanged will also consider new subset

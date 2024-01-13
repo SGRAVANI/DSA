9d97806ea62e6eb01c3bab1625a6441e50501034
SGRAVANI <shakuntla.ravani@gmail.com>
2024-01-13 23:59:41 +0530

    rodCutting

--- DSA/DSA-5/knapsack/unboundedKnapsack/rodCutting/rodCutting.js
+++ DSA/DSA-5/knapsack/unboundedKnapsack/rodCutting/rodCutting.js
@@ -6,3 +6,13 @@ for(let i=0;i<l;i++)
 }
 console.log("rod length array is : ",len)
 let price=[1,5,8,9,10,17,17,20]
+let dp=new Array(len+1);
+for(let i=0;i<dp.length;i++)
+{
+    dp[i]=new Array.fill(-1)
+}
+console.log(dp)
+function unboundedKnapsack(price,len,l)
+{
+    
+}

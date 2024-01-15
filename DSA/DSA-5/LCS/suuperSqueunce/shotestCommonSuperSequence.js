804a1af35d5278e0affa950ecdbebffb0cfa8ae7
SGRAVANI <shakuntla.ravani@gmail.com>
2024-01-15 23:52:35 +0530

    super sequence

--- DSA/DSA-5/LCS/suuperSqueunce/shotestCommonSuperSequence.js
+++ DSA/DSA-5/LCS/suuperSqueunce/shotestCommonSuperSequence.js
@@ -5,6 +5,9 @@
 // Input:   str1 = "geek",  str2 = "eke"
 // Output: "geeke"
 //super sequence: should contains both string sequetially in ordered way in merged string. it is not required continous both order should be maintained
+
+//note: should not be conitous but order should be maintained
 s1=AGGTAB
 s2=GXTXAYB
 super-string=AGGXTXAYB
+

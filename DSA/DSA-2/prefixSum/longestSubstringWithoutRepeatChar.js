262f6d7593ab7933105d8d27f6500e42f2a0c3ce
SGRAVANI <shakuntla.ravani@gmail.com>
2024-02-02 23:41:48 +0530

    longest substring uniqueCharacters brute force

--- DSA/DSA-2/prefixSum/longestSubstringWithoutRepeatChar.js
+++ DSA/DSA-2/prefixSum/longestSubstringWithoutRepeatChar.js
@@ -40,6 +40,9 @@ function isUnique(str)
     }
 }
 let str="aabcccbcb"
+//complexity : O(n^3)
+function bruteForceApproach(str)
+{
 let sub=""
 let maxL=0
 for(let i=0;i<str.length;i++)
@@ -57,3 +60,42 @@ for(let i=0;i<str.length;i++)
 }
 }
 console.log(maxL)
+}
+//bruteForceApproach(str)
+function usingMap(str)
+{
+    let m={}
+    let left=0;
+    let right=0;
+    let maxL=0
+    let s=''
+    while(right<str.length)
+    {
+    if(!m.hasOwnProperty(str[right]))
+    {
+        m[str[right]]=1;
+       // maxL=Math.max(maxL,right-left+1)
+       if(maxL<right-left+1)
+       {
+        maxL=right-left+1;
+        s=str.slice(left,right+1)
+       }
+        right++;
+          
+    //maxL=Math.max(maxL,)
+    
+  
+    }
+    else{
+    while(m.hasOwnProperty(str[right]))
+    {
+        delete m[str[left]];
+        left++;
+    }
+    }
+   
+    }
+    console.log(s)
+return maxL
+}
+console.log(usingMap(str))

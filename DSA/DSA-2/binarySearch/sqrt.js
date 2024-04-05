cf26d2da7c1fe8d7ba82e7a39c61a9050188d999
SGRAVANI <shakuntla.ravani@gmail.com>
2024-04-05 17:29:38 +0530

    sqrt

---
+++ DSA/DSA-2/binarySearch/sqrt.js
@@ -0,0 +1,24 @@
+let x=40
+let res;
+for(let i=1;i<=x;i++)
+{
+    let ans=i*i;
+    if(ans<=x)
+    {
+        res=i;
+    }
+    else{
+        break;
+    }
+}
+console.log(res)
+// let f;
+// for(let i=1;i<=p;i++)
+// {
+//     let n=res+i;
+//     if(n*n<=40)
+//     {
+//     f=n;
+//     }
+// }
+// console.log(f)

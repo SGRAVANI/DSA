b96ace2501dd69273dff373f97543c71eff9fc0a
SGRAVANI <shakuntla.ravani@gmail.com>
2024-04-07 01:11:39 +0530

    insertion sort

--- DSA/DSA-2/sort/insertion.js
+++ DSA/DSA-2/sort/insertion.js
@@ -1,2 +1,13 @@
 let a=[5,6,3,4,1,9]
-//in
+for(let i=1;i<a.length;i++)
+{let key=a[i]
+    let j=i-1;
+    while(j>=0 && key<a[j])
+    {
+        a[j+1]=a[j]
+        j--;
+    }
+    j++;
+    a[j]=key
+}
+console.log(a)
